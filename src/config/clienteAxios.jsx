363abea82d8a66c91ffd84659cf90f5a8998e950
DeinerSierra <deiner.sierra@gmail.com>
2023-04-22 23:22:40 -0500

    first commit

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -1,14 +1,7 @@
 import axios from 'axios'
-<<<<<<< HEAD
-const url = process.env.BACKEND_URL
-console.log(url)
-const clienteAxios = axios.create({
-    baseURL: url,
-=======
 
 const clienteAxios = axios.create({
     baseURL: process.env.BACKEND_URL,
->>>>>>> 8c012e5 (reintegro de version antigua)
     headers: {'Content-Type': 'application/json'}
 })
 
